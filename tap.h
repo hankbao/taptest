9b486c3b21f402001e91c747af2e41e2c0a5e122
hankbao <hankbao84@gmail.com>
2019-12-02 20:44:02 +0800

    add OpenVPN Copyright notice

--- tap.h
+++ tap.h
@@ -1,3 +1,23 @@
+/*
+ *  tapctl -- Utility to manipulate TUN/TAP interfaces on Windows
+ *            https://community.openvpn.net/openvpn/wiki/Tapctl
+ *
+ *  Copyright (C) 2018 Simon Rozman <simon@rozman.si>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2
+ *  as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #ifndef TAP_H
 #define TAP_H
 
